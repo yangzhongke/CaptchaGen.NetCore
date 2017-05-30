71580cb420da74ca02d5c12445488a3a65ad2a92
yangzhongke <yzk365@qq.com>
2017-05-30 10:11:52 +0800

    'init'

---
+++ CaptchaGen.NetCore.Tests/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace CaptchaGen.NetCore.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImageFactory.Distortion = 10;
+            using (FileStream fs = File.OpenWrite("d:/1.jpg"))
+            using (Stream picStream = ImageFactory.GenerateImage("AB123", 50, 100, 20))
+            {
+                picStream.CopyTo(fs);
+            }
+            Console.ReadKey();
+        }
+    }
+}
